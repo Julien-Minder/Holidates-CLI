a67d45d8059731def83931360760da19365759d1
Julien <julien.minder@gmail.com>
2020-05-07 12:55:00 +0200

    get-date-and-local-name

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ const {
     getCode
 } = require('country-list');
 
-/*if (process.argv[2] = undefined) {                     
-    console.log("Veuillez entrer un nom de pays valide (en anglais)")
-} else {
-    console.log(getCode(process.argv[2]))
-};*/
+
 let currentTime = new Date()
 let year = currentTime.getFullYear()
 let countryCode = getCode(process.argv[2])
+while (countryCode === undefined) {
+    console.log("Veuillez entrer un nom de pays valide (en anglais)"
 
-console.log(year)
+
+    )
+};
+
+
+/*console.log(year)
 console.log(countryCode);
-console.log('https://date.nager.at/api/v2/publicholidays/' + year + '/' + countryCode)
+console.log('https://date.nager.at/api/v2/publicholidays/' + year + '/' + countryCode)*/
 
 axios.get('https://date.nager.at/api/v2/publicholidays/' + year + '/' + countryCode)
     .then((response) => {
-        console.log(response.date)
-        console.log(response.name)
+        response.data.forEach(el => {
+            console.log(el.date)
+            console.log(el.localName)
+        });
+
     });
