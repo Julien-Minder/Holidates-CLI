6cfae86a0a3d59184ddad219644eb5b440d90b56
Julien <julien.minder@gmail.com>
2020-05-07 12:12:14 +0200

    api-implementation

--- index.js
+++ index.js
@@ -9,5 +9,16 @@ const {
 } else {
     console.log(getCode(process.argv[2]))
 };*/
+let currentTime = new Date()
+let year = currentTime.getFullYear()
+let countryCode = getCode(process.argv[2])
 
-console.log(getCode(process.argv[2]));
+console.log(year)
+console.log(countryCode);
+console.log('https://date.nager.at/api/v2/publicholidays/' + year + '/' + countryCode)
+
+axios.get('https://date.nager.at/api/v2/publicholidays/' + year + '/' + countryCode)
+    .then((response) => {
+        console.log(response.date)
+        console.log(response.name)
+    });
